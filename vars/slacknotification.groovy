f3f4c0d6eb487d6cbb1652698cb919fc32d1f0f4
vijaymatcha <matchavijay123@gmail.com>
2025-08-01 20:24:01 +0530

    Update slacknotification.groovy

--- vars/slacknotification.groovy
+++ vars/slacknotification.groovy
@@ -1,4 +1,4 @@
-def notifyBuild(String buildStatus = 'STARTED') {
+def call(String buildStatus = 'STARTED') {
   // build status of null means successful
   buildStatus =  buildStatus ?: 'SUCCESS'
 
